5aba449a7404003b1f793c41fa9d93bf05f52deb
LeeSanderson <lee.sanderson@codurance.com>
2025-07-07 16:16:00 +0100

    Basic bowling game scorer

---
+++ Kata.Src/Bowling/BowlingGameScorer.cs
@@ -0,0 +1,36 @@
+﻿namespace Kata.Src.Bowling;
+
+public class BowlingGameScorer
+{
+    private int currentRoll;
+    private readonly int[] rolls = new int[21];
+    public void Roll(int pins)
+    {
+        // TODO: Check if currentRoll is within bounds
+        // TODO: Check if pins is within bounds (0 to 10)
+        // TODO: Check if pins in frame is valid (e.g., not more than 10 in a single frame)
+        rolls[currentRoll++] = pins;
+    }
+
+    public void Roll(int firstPins, params int[] pins)
+    {
+        Roll(firstPins);
+        foreach (var pin in pins)
+        {
+            Roll(pin);
+        }
+    }   
+
+    public int Score
+    {
+        get
+        {
+            var score = 0;
+            for (var i = 0; i < currentRoll; i++)
+            {
+                score += rolls[i];
+            }
+            return score;
+        }
+    }
+}
---
+++ Kata.Test/Bowling/BowlingGameScorerShould.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Kata.Src.Bowling;
+using Xunit;
+
+namespace Kata.Test.Bowling;
+
+public class BowlingGameScorerShould
+{
+    [Fact]
+    public void ScoreZeroForNoRolls()
+    {
+        var game = new BowlingGameScorer();
+        game.Score.Should().Be(0);
+    }
+
+    [Fact]
+    public void ScoreZeroForGutterGame()
+    {
+        var game = new BowlingGameScorer();
+        for (var i = 0; i < 20; i++)
+        {
+            game.Roll(0);
+        }
+        game.Score.Should().Be(0);
+    }
+
+    [Fact]
+    public void ScoreOneForSingleRollOfOne()
+    {
+        var game = new BowlingGameScorer();
+        game.Roll(1, 0);
+        game.Score.Should().Be(1);
+    }
+}
